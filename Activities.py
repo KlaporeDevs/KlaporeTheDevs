503f159519da4c72bdecc4f9f8f3f077b5a009fb
KlaporeDevs <125471747+KlaporeDevs@users.noreply.github.com>
2023-08-23 19:17:37 +0800

    Add files via upload

---
+++ Activities.py
@@ -0,0 +1,27 @@
+#Janrich Natavio Verbo BSIT1-02
+#Activities
+
+#No1 Activity
+Name = "I Love Python 3.8"
+print(Name)
+#No2 Activity
+Price = ("$42.99")
+Weight = ("30 lbs")
+print("Price: ", Price)
+print("Weight:",Weight)
+#No3 Activity
+capital = 1000
+years = 5
+rate = 0.03
+Future_Value = capital *(1 + rate) ** years
+Total = round(Future_Value, 2)
+print(f"The Future Value Of The Investment: {Total} USD")
+#No4 Activity
+Name = input("What Is Your Name:")
+print("Hello Sir/Ma'am", Name)
+#No5 Activity
+Weather1 = input("Enter The First Season:")
+Weather2 = input("Enter The Second Season:")
+Weather3 = input("Enter The Third Season:")
+Weather4 = input("Enter The Fourth Season:")
+print("You Have Entered The Following And Those Are",Weather1, Weather2, Weather3, "and", Weather4)
